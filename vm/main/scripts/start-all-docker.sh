b30d9f148c71d39e9bceb378fa11d2780475516f
aleung241 <anthoynleung989@gmail.com>
2021-11-17 23:01:22 +1300

    Added general docker-compose to start script

--- vm/main/scripts/start-all-docker.sh
+++ vm/main/scripts/start-all-docker.sh
@@ -3,12 +3,14 @@
 BASE_DIRECTORY="/opt/docker-compose"
 
 docker-compose -f $BASE_DIRECTORY/media/docker-compose.yml pull
+docker-compose -f $BASE_DIRECTORY/general/docker-compose.yml pull
 docker-compose -f $BASE_DIRECTORY/reverse-proxy/docker-compose.yml pull
 docker-compose -f $BASE_DIRECTORY/special/docker-compose.yml pull
 docker-compose -f $BASE_DIRECTORY/torrent/docker-compose.yml pull
 
 
 docker-compose -f $BASE_DIRECTORY/media/docker-compose.yml up -d
+docker-compose -f $BASE_DIRECTORY/general/docker-compose.yml up -d
 docker-compose -f $BASE_DIRECTORY/reverse-proxy/docker-compose.yml up -d
 docker-compose -f $BASE_DIRECTORY/special/docker-compose.yml up -d
 docker-compose -f $BASE_DIRECTORY/torrent/docker-compose.yml up -d
