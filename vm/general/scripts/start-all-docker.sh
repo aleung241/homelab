dbbeba99f39c31ed9914cd8092eabb741a523dfd
aleung241 <anthoynleung989@gmail.com>
2026-02-19 19:21:09 +1300

    Removal of `general` machine

--- vm/general/scripts/start-all-docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-BASE_DIRECTORY="/opt/docker-compose"
-
-docker-compose $(find $BASE_DIRECTORY -maxdepth 1 -type f -name '*.yml' -printf "-f $BASE_DIRECTORY/%f ") pull
-docker-compose $(find $BASE_DIRECTORY -maxdepth 1 -type f -name '*.yml' -printf "-f $BASE_DIRECTORY/%f ") up -d
