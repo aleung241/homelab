28db04ae337a4113da19b151fe060725d5592d01
aleung241 <anthoynleung989@gmail.com>
2026-08-18 16:31:47 +1200

    Update docker start script

--- vm/plex-new/scripts/start-all-docker.sh
+++ vm/plex-new/scripts/start-all-docker.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+set -euo pipefail
 
-BASE_DIRECTORY="/opt/docker-compose"
+BASE_DIRECTORY="/opt/docker"
 
-docker-compose $(find $BASE_DIRECTORY -maxdepth 1 -type f -name '*.yml' -printf "-f $BASE_DIRECTORY/%f ") pull
-docker-compose $(find $BASE_DIRECTORY -maxdepth 1 -type f -name '*.yml' -printf "-f $BASE_DIRECTORY/%f ") up -d
+compose_files=$(find "$BASE_DIRECTORY" -maxdepth 1 -type f -name '*.yml' | sed 's|^|-f |')
+
+docker compose $compose_files pull
+docker compose $compose_files up -d
