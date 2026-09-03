3213adff880ae2a9d65c86f3955ebb32c78189f0
aleung241 <anthoynleung989@gmail.com>
2026-09-03 16:18:36 +1200

    Allow docker compose up -d to run even if pull fails

--- vm/main/scripts/start-all-docker.sh
+++ vm/main/scripts/start-all-docker.sh
@@ -5,5 +5,5 @@ BASE_DIRECTORY="/opt/docker"
 
 compose_files=$(find "$BASE_DIRECTORY" -maxdepth 1 -type f -name '*.yml' | sed 's|^|-f |')
 
-docker compose $compose_files pull
+docker compose $compose_files pull || true
 docker compose $compose_files up -d
